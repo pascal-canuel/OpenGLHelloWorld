c868930dfa51716eae5ad5eaa17fcf3f5cdd08f4
Pascal <pascalcanuel@hotmail.com>
2018-08-29 13:06:23 -0400

    change background color if key pressed

--- OpenGLHelloWorld/main.cpp
+++ OpenGLHelloWorld/main.cpp
@@ -2,11 +2,44 @@
 #include "Dependencies\freeglut\freeglut.h"
 #include <iostream>
 
+//	Fonction de rendu
 void renderScene()
 {
 	glClearColor(0, 0, 255, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
+}
+
+//	Fonction de rappel pour capture les touches du clavier
+void getKeyStroke(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		case 'r':
+			glClearColor(255, 0, 0, 0);
+			break;
+		case 'b':
+			glClearColor(0, 0, 255, 0);
+			break;
+		case 'v':
+			glClearColor(0, 255, 0, 0);
+			break;
+		case 'n':
+			glClearColor(0, 0, 0, 0);
+			break;
+		case 'm':
+			glClearColor(127, 255, 255, 0);
+			break;
+		case 't':
+			glClearColor(64, 224, 208, 0);
+			break;
+		case 'o':
+			glClearColor(255, 128, 0, 0);
+			break;
+		default:
+			glClearColor(255, 255, 255, 0);
+			break;
+	}
 }
 
 int main(int argc, char **argv)
@@ -17,11 +50,13 @@ int main(int argc, char **argv)
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Laboratoire 1");
 
-	//	enregistre la fonction de rendu
+	//	enregistre les fonctions de rappel
 	glutDisplayFunc(renderScene);
+	glutKeyboardFunc(getKeyStroke);
 
 	//	démarre la boucle de traitement des événements 
 	glutMainLoop();
 
 	return 0;
 }
+
