4d65618d251c64178dcc693b8ac08a27573e764c
Pascal <pascalcanuel@hotmail.com>
2018-08-29 12:52:53 -0400

    render blue screen

--- OpenGLHelloWorld/main.cpp
+++ OpenGLHelloWorld/main.cpp
@@ -2,13 +2,26 @@
 #include "Dependencies\freeglut\freeglut.h"
 #include <iostream>
 
+void renderScene()
+{
+	glClearColor(0, 0, 255, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
 int main(int argc, char **argv)
 {
-	//Écrire ici le code d’initialisation
+	//	Écrire ici le code d’initialisation
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Laboratoire 1");
+
+	//	enregistre la fonction de rendu
+	glutDisplayFunc(renderScene);
+
+	//	démarre la boucle de traitement des événements 
+	glutMainLoop();
 
 	return 0;
 }
