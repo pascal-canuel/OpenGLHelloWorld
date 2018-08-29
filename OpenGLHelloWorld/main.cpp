cef446c6e36934e9ccce47cb3ea4ef9dcd2e217c
Pascal <pascalcanuel@hotmail.com>
2018-08-29 12:37:33 -0400

    first commit

---
+++ OpenGLHelloWorld/main.cpp
@@ -0,0 +1,14 @@
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	//Écrire ici le code d’initialisation
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Laboratoire 1");
+
+	return 0;
+}
